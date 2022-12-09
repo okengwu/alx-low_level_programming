115e5e16bfbdd904c7d135e8a6e6fd41481f08dc
okengwu <okengwuchidiebere@yahoo.com>
2022-12-08 21:32:23 -0800

    multilimgual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * use the function puts
+ * You are not allowed to use printf
+ * Your program should end with the value 
+ *
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
