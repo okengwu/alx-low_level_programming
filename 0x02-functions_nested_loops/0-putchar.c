8278be5c5c1250aacc932cab1a284fe1dc1532d8
okengwu <okengwuchidiebere@yahoo.com>
2022-12-15 23:51:03 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include "main.h"
+
 /**
  * main - print _putchar
  *
- * Return: 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	char word[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 9; i++)
-		putchar(word[i]);
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
